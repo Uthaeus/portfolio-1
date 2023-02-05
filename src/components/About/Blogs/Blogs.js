2e29bc342ff484f8652ba4cc6382319a0fca0cdc
Uthaeus <romanlavery@gmail.com>
2023-02-05 11:28:49 -0700

    created initial pages for about, contact, blogs

---
+++ src/components/About/Blogs/Blogs.js
@@ -0,0 +1,12 @@
+
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1>Blogs Page</h1>
+        </div>
+    );
+};
+
+
+export default Blogs;
---
+++ src/components/ContactMe/ContactMe.js
@@ -0,0 +1,12 @@
+
+
+const ContactMe = () => {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+        </div>
+    );
+};
+
+
+export default ContactMe;
