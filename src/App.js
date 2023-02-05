7fecda154c96cc0cb2d7ee6861e4b99dc884850b
Uthaeus <romanlavery@gmail.com>
2023-02-05 11:08:14 -0700

    created initial Header component for navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hello</h1>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+
+import classes from './Header.module.css';
+
+const Header = props => {
+
+    return (
+        <div className={classes.header}>
+            <p>Header/Nav</p>
+        </div>
+    );
+};
+
+
+export default Header;
