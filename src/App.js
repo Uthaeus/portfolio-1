20970f1e0fff2aafeb92c0b9c90d051b5a7d352d
Uthaeus <romanlavery@gmail.com>
2023-02-05 14:57:09 -0700

    set up switching and routing for page navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from "./components/layout/Layout";
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import ContactMe from './components/ContactMe/ContactMe';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
-    <div>
-      <Layout />
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/contact-me'>
+          <ContactMe />
+        </Route>
+        <Route path='/blogs'>
+          <Blogs />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
