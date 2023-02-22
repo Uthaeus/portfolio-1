6b1bd6f2ffb7e95346c8be435ad75918ee88f8bc
Uthaeus <romanlavery@gmail.com>
2023-02-21 21:07:49 -0700

    small fix in Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,19 @@
 
 import classes from './Projects.module.css';
 
-const Projects = () => {
+const Projects = (props) => {
     // when logged in be able to add a new project
+    
+
+    const addProject = <button>Add New Project</button>;
 
     return (
         <div className={classes.projects}>
             <h1>Projects Page</h1>
+            {props.isLogged ? addProject : null}
+            <div>
+
+            </div>
         </div>
     );
 };
